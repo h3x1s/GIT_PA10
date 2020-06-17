e8ab43a853b4eb128fdbc21f6d3451475e8170b6
h3x1s <64124310+h3x1s@users.noreply.github.com>
2020-06-17 21:12:25 +0800

    Updated Prototype
    Not Finished Yet

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
-
+    private float JumpPower = 10f;
+    Rigidbody player;
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
@@ -15,6 +17,23 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.velocity = new Vector3(0f, JumpPower, 0f);
             thisAnimation.Play();
+        }
+        
+            
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "Obstacle")
+        {
+            
+            SceneManager.LoadScene("GameOver");
+            if(Input.GetKey(KeyCode.R))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
     }
 }
